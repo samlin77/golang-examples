19eebdba47a9f716bf0110e5f553559eb0b521f0
Sam Lin <sam_lin@trendmicro.com>
2025-06-13 15:01:11 +0800

    Add user input echo to HelloWorld

--- beginner/HelloWorld.go
+++ beginner/HelloWorld.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	// print string
-	print("Hello World\n")
+	print("Hello Sam\n")
+
+	// print numbers from 1 to 100
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
+
+	// wait for user input and echo it
+	fmt.Print("Enter text: ")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Printf("You entered: %s\n", input)
 }
