0962adb01df41f1a4744ec8ef6f5bbb4eb440045
Sam Lin <sam_lin@trendmicro.com>
2025-06-13 14:53:48 +0800

    Add number printing example

--- beginner/HelloWorld.go
+++ beginner/HelloWorld.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	// print string
-	print("Hello World\n")
+	print("Hello Sam\n")
+
+	// print numbers from 1 to 100
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
 }
